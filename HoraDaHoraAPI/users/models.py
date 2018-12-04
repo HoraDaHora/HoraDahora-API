8d8de96a32d65c92c5235ca2f8b415444ab878ea
Lucas Vitor de Paula <lucasvitorifg@gmail.com>
2018-12-04 21:35:21 -0200

    Mudando default de coins

--- HoraDaHoraAPI/users/models.py
+++ HoraDaHoraAPI/users/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.IntegerField(null=True)
     photo = models.ImageField(upload_to='./media', null=True)
-    coins = models.IntegerField(default = 0)
+    coins = models.IntegerField(default = 20)
     points = models.IntegerField(default = 0)
     # aulas que tem q dar
     # aulas que recebeu
